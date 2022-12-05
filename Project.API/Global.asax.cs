cedb5d2650f2a141a1b336a9ddc7b59303e4d27d
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-12-06 01:08:23 +0300

    API and MVCUI layers updates

--- Project.API/Global.asax.cs
+++ Project.API/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Project.API
@@ -13,7 +11,6 @@ namespace Project.API
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
--- Project.MVCUI/Properties/AssemblyInfo.cs
+++ Project.MVCUI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6a36f024-6da4-446c-a1d4-27e694178fd7")]
+[assembly: Guid("3f1f3283-28cc-4e74-97df-a9b0e88408c3")]
 
 // Version information for an assembly consists of the following four values:
 //
